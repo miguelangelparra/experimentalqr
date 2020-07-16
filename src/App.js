7fc2832f03668eeeaf3fc6afbf049b7ee2beb28a
miguelangelparra <miguelangel.parradavila@gmail.com>
2020-07-16 19:59:58 -0300

    inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TestQR} from "./components/QR"
+import qrBattleship from "./qrBattleship.png"
+
 
 function App() {
   return (
+    <div>        <h1>Miguelangel ----- Escanea el QR para Jugar!</h1>
+   
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={qrBattleship} />
+
+     <TestQR></TestQR>
+         
       </header>
+    </div>
     </div>
   );
 }
